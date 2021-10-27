b93033b19aecdbd638fbca5246bdbd00c63884b4
Waischbrot <92989284+Waischbrot@users.noreply.github.com>
2021-10-27 18:24:14 +0200

    First Version
    Kreiseljustus ist doof ._.

---
+++ Lobbysystem/src/main/java/de/rubymc/lobbysystem/listener/ItemClickListener.java
@@ -0,0 +1,13 @@
+package de.rubymc.lobbysystem.listener;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public class ItemClickListener implements Listener {
+
+    @EventHandler
+    public void onInteract(PlayerInteractEvent event) {
+
+    }
+}
---
+++ Lobbysystem/src/main/java/de/rubymc/lobbysystem/listener/PlayerInventory.java
@@ -0,0 +1,15 @@
+package de.rubymc.lobbysystem.listener;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class PlayerInventory implements Listener {
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+
+    }
+}
