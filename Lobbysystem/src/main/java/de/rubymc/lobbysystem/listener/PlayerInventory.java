0da112272c6e3f2b87d3e1083c4aac595808bb56
Waischbrot <92989284+Waischbrot@users.noreply.github.com>
2021-10-31 08:37:42 +0100

    Cleaner lobbyitems ^^

--- Lobbysystem/src/main/java/de/rubymc/lobbysystem/listener/PlayerInventory.java
+++ Lobbysystem/src/main/java/de/rubymc/lobbysystem/listener/PlayerInventory.java
@@ -2,12 +2,10 @@ package de.rubymc.lobbysystem.listener;
 
 import de.rubymc.lobbysystem.util.ItemBuilder;
 
-import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerGameModeChangeEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class PlayerInventory implements Listener {
@@ -19,25 +17,33 @@ public class PlayerInventory implements Listener {
         //Löscht das Spielerinv
         player.getInventory().clear();
 
+        for (int i = 9; i <= 35; i++) {
+            player.getInventory().setItem(i, new ItemBuilder(Material.GRAY_STAINED_GLASS_PANE).setDisplayName("§c").build());
+        }
+        //Socialmedia Items
+        player.getInventory().setItem(20, new ItemBuilder(Material.PLAYER_HEAD).setDisplayName("§6Instagram §8× §7Folgen").getSkull("http://textures.minecraft.net/texture/25b3f2cfa0739c4e828316f39f90b05bc1f4ed27b1e35888511f558d4675").build());
+        player.getInventory().setItem(22, new ItemBuilder(Material.PLAYER_HEAD).setDisplayName("§bDiscord §8× §7Beitreten").getSkull("http://textures.minecraft.net/texture/4d42337be0bdca2128097f1c5bb1109e5c633c17926af5fb6fc20000011aeb53").build());
+        player.getInventory().setItem(24, new ItemBuilder(Material.PLAYER_HEAD).setDisplayName("§6Website §8× §7Klick").getSkull("http://textures.minecraft.net/texture/b3175286cd3ba1a3a9c9082977d09dd37b177abf37a466580c27d1dee72b3c18").build());
+
         //Navigator Item
-        player.getInventory().setItem(4, new ItemBuilder(Material.COMPASS).setDisplayName("§bNavigator §8× §7Rightclick").setGlow(true).build());
+        player.getInventory().setItem(4, new ItemBuilder(Material.COMPASS).setDisplayName("§bNavigator §8× §7Rechtsklick").setGlow(true).build());
 
         //Profil
-        player.getInventory().setItem(8, new ItemBuilder(Material.PLAYER_HEAD, (short) 3).setHead(player.getName()).setDisplayName("§bProfile §8× §7Rightclick").build());
+        player.getInventory().setItem(8, new ItemBuilder(Material.PLAYER_HEAD, (short) 3).setHead(player.getName()).setDisplayName("§bProfil §8× §7Rechtsklick").build());
 
         //Playerhider
         player.getInventory().setItem(5, new ItemBuilder(Material.LIME_DYE).setDisplayName("§bPlayerhider §8× §aAlle").build());
 
         //Cosmetics
-        player.getInventory().setItem(0, new ItemBuilder(Material.CHEST).setDisplayName("§bCosmetics §8× §7Rightclick").build());
+        player.getInventory().setItem(0, new ItemBuilder(Material.CHEST).setDisplayName("§bKosmetik §8× §7Rechtsklick").build());
 
         //Gadgetslot
-        player.getInventory().setItem(1, new ItemBuilder(Material.GRAY_DYE).setDisplayName("§bGadgets §8× §cEmpty").build());
+        player.getInventory().setItem(1, new ItemBuilder(Material.GRAY_DYE).setDisplayName("§bGadgets §8× §cLeer").build());
 
         //Settings
-        player.getInventory().setItem(7, new ItemBuilder(Material.PLAYER_HEAD).setDisplayName("§bSettings §8× §7Rightclick").getSkull("http://textures.minecraft.net/texture/e4d49bae95c790c3b1ff5b2f01052a714d6185481d5b1c85930b3f99d2321674").build());
+        player.getInventory().setItem(7, new ItemBuilder(Material.PLAYER_HEAD).setDisplayName("§bEinstellungen §8× §7Rechtsklick").getSkull("http://textures.minecraft.net/texture/e4d49bae95c790c3b1ff5b2f01052a714d6185481d5b1c85930b3f99d2321674").build());
 
         //Lobbyswitcher
-        player.getInventory().setItem(3, new ItemBuilder(Material.NETHER_STAR).setDisplayName("Lobbyswitcher §8× §7Rightclick").build());
+        player.getInventory().setItem(3, new ItemBuilder(Material.NETHER_STAR).setDisplayName("§eLobbyswitcher §8× §7Rechtsklick").build());
     }
 }
