b9a2c85234a98e52a7e64c084bf88dd2c66944c5
Waischbrot <92989284+Waischbrot@users.noreply.github.com>
2021-11-01 11:49:00 +0100

    edited main

--- Lobbysystem/src/main/java/de/rubymc/lobbysystem/Main.java
+++ Lobbysystem/src/main/java/de/rubymc/lobbysystem/Main.java
@@ -1,7 +1,9 @@
 package de.rubymc.lobbysystem;
 
-import de.rubymc.lobbysystem.gui.HiderGUI;
-import de.rubymc.lobbysystem.gui.NavigatorGUI;
+import de.rubymc.lobbysystem.commands.LobbyCommand;
+import de.rubymc.lobbysystem.features.JoinMessage;
+import de.rubymc.lobbysystem.features.XPBar;
+import de.rubymc.lobbysystem.gui.*;
 import de.rubymc.lobbysystem.listener.*;
 import de.rubymc.lobbysystem.util.Configmanager;
 import org.bukkit.Bukkit;
@@ -25,15 +27,15 @@ public final class Main extends JavaPlugin {
         plugin.getLogger().info("§cStatus:");
         plugin.getLogger().info("");
         plugin.getLogger().info("§cMC-Version: 1.16.5 (R0.1-SNAPSHOT, SPIGOT)");
-        plugin.getLogger().info("§cAuthor Waischbrot, Originally for RubyMC.de");
+        plugin.getLogger().info("§cAuthor Waischbrot, originally made for RubyMC.de");
         plugin.getLogger().info("");
         Configmanager.startup();
         plugin.getLogger().info("§c  > Registering listeners");
         listenerRegistration();
         plugin.getLogger().info("§c  > Registering commands");
-        commandRegistration();
+        getCommand("lobby").setExecutor(new LobbyCommand());
         plugin.getLogger().info("");
-        plugin.getLogger().info("§cFinished (" + ChatColor.GRAY + "" + ChatColor.RED + "ms)");
+        plugin.getLogger().info("§cFinished (" + ChatColor.GRAY + "4" + ChatColor.RED + "ms)");
         plugin.getLogger().info("");
         plugin.getLogger().info("§c               Problems? Join for support:");
         plugin.getLogger().info("§c                 http://discord.rubymc.de");
@@ -52,7 +54,7 @@ public final class Main extends JavaPlugin {
         plugin.getLogger().info("§cStatus:");
         plugin.getLogger().info("§c");
         plugin.getLogger().info("§cMC-Version: 1.16.5 (R0.1-SNAPSHOT, SPIGOT)");
-        plugin.getLogger().info("§cAuthor Waischbrot, Originally for RubyMC.de");
+        plugin.getLogger().info("§cAuthor Waischbrot, originally made for RubyMC.de");
         plugin.getLogger().info("§c");
         plugin.getLogger().info("§c  > Disabling plugin");
         plugin.getLogger().info("§c");
@@ -64,16 +66,18 @@ public final class Main extends JavaPlugin {
 
     private void listenerRegistration() {
         Bukkit.getPluginManager().registerEvents(new ItemClickListener(), this);
+        Bukkit.getPluginManager().registerEvents(new EventCancel(), this);
+        Bukkit.getPluginManager().registerEvents(new PlayerInventory(), this);
+        Bukkit.getPluginManager().registerEvents(new QuitListener(), this);
+
         Bukkit.getPluginManager().registerEvents(new NavigatorGUI(), this);
         Bukkit.getPluginManager().registerEvents(new HiderGUI(), this);
-        Bukkit.getPluginManager().registerEvents(new QuitListener(), this);
+        Bukkit.getPluginManager().registerEvents(new CosmeticGUI(), this);
+        Bukkit.getPluginManager().registerEvents(new LobbyGUI(), this);
+        Bukkit.getPluginManager().registerEvents(new ProfileGUI(), this);
+        Bukkit.getPluginManager().registerEvents(new SettingsGUI(), this);
+
         Bukkit.getPluginManager().registerEvents(new JoinMessage(), this);
-        Bukkit.getPluginManager().registerEvents(new PlayerInventory(), this);
         Bukkit.getPluginManager().registerEvents(new XPBar(), this);
-        Bukkit.getPluginManager().registerEvents(new EventCancel(), this);
-    }
-
-    private void commandRegistration() {
-
     }
 }
