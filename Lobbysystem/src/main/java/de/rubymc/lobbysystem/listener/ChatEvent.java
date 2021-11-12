b1c5b9260d88f9eec37ebbbbb84b5d96940bbb73
Waischbrot <92989284+Waischbrot@users.noreply.github.com>
2021-11-12 18:50:52 +0100

    removed emojisystem

--- Lobbysystem/src/main/java/de/rubymc/lobbysystem/listener/ChatEvent.java
+++ Lobbysystem/src/main/java/de/rubymc/lobbysystem/listener/ChatEvent.java
@@ -16,49 +16,6 @@ public class ChatEvent implements Listener {
         String message = ChatColor.translateAlternateColorCodes('&', event.getMessage());
         String rang = PlaceholderAPI.setPlaceholders(player, "%vault_prefix%");
 
-        message = message.replaceAll("<3", "❤");
-        message = message.replaceAll("->", "➜");
-        message = message.replaceAll(":star:", "✪");
-        message = message.replaceAll(":pen:", "✎");
-        message = message.replaceAll(":phone:", "☎");
-        message = message.replaceAll(":xx:", "✖");
-        message = message.replaceAll(":a:", "Ⓐ");
-        message = message.replaceAll(":b:", "Ⓑ");
-        message = message.replaceAll(":c:", "Ⓒ");
-        message = message.replaceAll(":d:", "Ⓓ");
-        message = message.replaceAll(":e:", "Ⓔ");
-        message = message.replaceAll(":f:", "Ⓕ");
-        message = message.replaceAll(":g:", "Ⓖ");
-        message = message.replaceAll(":h:", "Ⓗ");
-        message = message.replaceAll(":i:", "Ⓘ");
-        message = message.replaceAll(":j:", "Ⓙ");
-        message = message.replaceAll(":k:", "Ⓚ");
-        message = message.replaceAll(":l:", "Ⓛ");
-        message = message.replaceAll(":m:", "Ⓜ");
-        message = message.replaceAll(":n:", "Ⓝ");
-        message = message.replaceAll(":o:", "Ⓞ");
-        message = message.replaceAll(":p:", "Ⓟ");
-        message = message.replaceAll(":q:", "Ⓠ");
-        message = message.replaceAll(":r:", "Ⓡ");
-        message = message.replaceAll(":s:", "Ⓢ");
-        message = message.replaceAll(":t:", "Ⓣ");
-        message = message.replaceAll(":u:", "Ⓤ");
-        message = message.replaceAll(":v:", "Ⓥ");
-        message = message.replaceAll(":w:", "Ⓦ");
-        message = message.replaceAll(":x:", "Ⓧ");
-        message = message.replaceAll(":y:", "Ⓨ");
-        message = message.replaceAll(":z:", "Ⓩ");
-        message = message.replaceAll(":1:", "➀");
-        message = message.replaceAll(":2:", "➁");
-        message = message.replaceAll(":3:", "➂");
-        message = message.replaceAll(":4:", "➃");
-        message = message.replaceAll(":5:", "➄");
-        message = message.replaceAll(":6:", "➅");
-        message = message.replaceAll(":7:", "➆");
-        message = message.replaceAll(":8:", "➇");
-        message = message.replaceAll(":9:", "➈");
-        message = message.replaceAll(":10:", "➉");
-
         event.setFormat(rang + player.getName() + " §8➽ §7" + message);
     }
 }
